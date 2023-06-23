2227ff8b9f9b9c2f80be830d7320e7ee872000cb
Prasham1710 <jainprasham17@gmail.com>
2023-06-23 13:48:35 +0530

    page done

--- app/page.js
+++ app/page.js
@@ -1,4 +1,16 @@
-
+import Link from "next/link"
 export default function Home() {
-  return <>hello</>
+  return (
+  <div className="h-screen w-full ">
+    <div className="text-center w-4/5 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+    <h1> Explore the food around the world</h1>
+      <Link
+          className="shadow-gray-50 bg-gray-300 rounded text-xl py-2 px-4 cursor-pointer hover:bg-blue-500 hover:text-white"
+          href="/types"
+        >
+          List of Cuisines
+        </Link>
+    </div>
+  </div>
+  )
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='py-5 bg-slate-300'>
+        <div>
+            <Link href="/">
+                <h1 className=' text-purple text-blue-700 font-bold text-5xl text-center">'> Foode </h1>
+            </Link>
+        </div>
+    </div>
   )
 }
 

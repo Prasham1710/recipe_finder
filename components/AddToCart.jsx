da1a45e1f6369d79793995294c54ace1397011ad
Prasham1710 <jainprasham17@gmail.com>
2023-06-23 17:16:01 +0530

    Cart

--- components/AddToCart.jsx
+++ components/AddToCart.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+"use client";
 
-const AddToCart = () => {
+import React from "react";
+
+const AddToCart = ({ name }) => {
   return (
-    <div>AddToCart</div>
-  )
-}
+    <button
+      className="text-white bg-red-500   hover:bg-red-600 rounded py-1 px-3 mt-5 client"
+      onClick={() => {
+        alert(`Added ${name} to cart`);
+      }}
+    >
+      Add to Cart
+    </button>
+  );
+};
 
-export default AddToCart
+export default AddToCart;
